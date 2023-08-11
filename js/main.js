1d609cd02dc750d8705453d9f024aef914b996bc
Roberto Kyuchukov <robertokyuchukov@Robertos-iMac.local>
2023-08-11 15:07:09 +0300

    end stage of project

--- js/main.js
+++ js/main.js
@@ -1,84 +1,127 @@
- class Player{
- constructor(){
-    this.positionX = 50;
-    this.positionY = 0;
-    this.width = 20;
-    this.height = 20;
-    this.domElement = null;
-    this.createDomElement();
- }
- createDomElement(){
-    this.domElement = document.createElement("div");
-    this.domElement.id = "player";
-    this.domElement.style.height = this.height + "vh";
-    this.domElement.style.width = this.width + "vw";
-    this.domElement.style.left = this.positionX + "vw";
-    this.domElement.style.bottom = this.positionY + "vh";
-    const parentElm = document.getElementById("board");
-    parentElm.appendChild(this.domElement);
- }
- moveLeft(){
-    this.positionX--;
-    this.domElement.style.left = this.positionX + "vw";
-    
- }
- moveRight(){
-    this.positionX++;
-    this.domElement.style.left = this.positionX + "vw";
- }
+class Game {
+    constructor() {
+        this.player = new Player();
+        this.obstaclesArr = []; //will store instances of the class Obstacle
+    }
+    start() {
+
+        // attach event listeners
+        this.attachEventListeners();
+
+        // create obstacles
+        setInterval(() => {
+            const newObstacle = new Obstacle();
+            this.obstaclesArr.push(newObstacle);
+        }, 3000);
+
+        // move all obstacles
+        setInterval(() => {
+            this.obstaclesArr.forEach((obstacleInstance) => {
+                obstacleInstance.moveDown(); // move
+                this.removeObstacleIfOutside(obstacleInstance); // remove if outside
+                this.detectCollision(obstacleInstance); // detect collision
+
+            });
+        }, 100);
+    }
+    attachEventListeners() {
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "ArrowLeft") {
+                this.player.moveLeft();
+            } else if (event.key === "ArrowRight") {
+                this.player.moveRight();
+            }
+        });
+    }
+    removeObstacleIfOutside(obstacleInstance){
+        if (obstacleInstance.positionY < 0 - obstacleInstance.height) {
+            obstacleInstance.domElement.remove(); //remove from the dom
+            this.obstaclesArr.shift(); // remove from the array
+        }
+    }
+    detectCollision(obstacleInstance){
+        if (
+            this.player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            this.player.positionX + this.player.width > obstacleInstance.positionX &&
+            this.player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            this.player.positionY + this.player.height > obstacleInstance.positionY
+        ) {
+            // Collision detected!
+            console.log("game over my fren! ");
+            //location.href = "./gameover.html";
+        }
+    }
 }
 
 
-class Obstacle{
-    constructor(){
-    this.positionX = 50;
-    this.positionY = 100;
-    this.width = 20;
-    this.height = 10;
-    this.domElement = null;
+class Player {
+    constructor() {
+        this.width = 20;
+        this.height = 10;
+        this.positionX = 50 - (this.width / 2);
+        this.positionY = 0;
+        this.domElement = null;
 
-    this.createDomElement();
+        this.createDomElement();
     }
-    createDomElement(){
+    createDomElement() {
+        // create dom element
         this.domElement = document.createElement("div");
-        this.domElement.className = "obstacle";
+
+        // set id
+        this.domElement.id = "player";
         this.domElement.style.width = this.width + "vw";
         this.domElement.style.height = this.height + "vh";
-        this.domElement.style.bottom = this.positionY + "vh";
         this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
 
+        //append to the dom
         const parentElm = document.getElementById("board");
         parentElm.appendChild(this.domElement);
     }
-    moveDown(){
-        console.log("move down");
-            this.positionY -= 10;
-            this.domElement.style.bottom = this.positionY + "vh";
+    moveLeft() {
+        this.positionX--;
+        this.domElement.style.left = this.positionX + "vw";
+    }
+    moveRight() {
+        this.positionX++;
+        this.domElement.style.left = this.positionX + "vw";
     }
 }
 
-const player = new Player();
 
-const obstaclesArr = [];
+class Obstacle {
+    constructor() {
+        this.width = 20;
+        this.height = 10;
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1)); // random number between 0 and (100 - width)
+        this.positionY = 100;
+        this.domElement = null;
 
-setInterval(() => {
-    const obstacleOne = new Obstacle();
-    obstaclesArr.push(obstacleOne);
-    console.log("one object");
-}, 2000);
+        this.createDomElement();
+    }
+    createDomElement() {
+        // create dom element
+        this.domElement = document.createElement("div");
 
-setInterval(() => {
-        obstaclesArr.forEach( elem => {
-            elem.moveDown();
-        });
-}, 200);
+        // set id
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
 
-document.addEventListener("keydown", event => {
-    if(event.key === "ArrowRight"){
-        console.log("r");
-        player.moveRight();
-    } else if(event.key === "ArrowLeft"){
-        console.log("l");
-        player.moveLeft();
-    }   
-})
+        //append to the dom
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+    }
+    moveDown() {
+        this.positionY -= 2;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+}
+
+
+const game = new Game();
+game.start();
+
